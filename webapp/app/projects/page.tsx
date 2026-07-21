5113839150a0159d1b89d687d2162afca44d3daa
sneakylizard123-4 <sneakylizard123_4@hotmail.com>
2026-07-20 17:42:20 -0700

    prepare for github pages

--- webapp/app/projects/page.tsx
+++ webapp/app/projects/page.tsx
@@ -1,5 +1,12 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function ProjectsPage() {
-  redirect("/projects/featured");
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/projects/featured/");
+  }, [router]);
+  return null;
 }
